02a247f655742795e0742cd12fc6b510dd61fd33
Moksh Khurana <moksh.khurana@gmail.com>
2016-12-18 15:25:58 -0500

    Readme updated Added an example for getLists method.

--- MailchimpApplication/app/src/main/java/com/mokshkhurana/mailchimp/application/MainActivity.java
+++ MailchimpApplication/app/src/main/java/com/mokshkhurana/mailchimp/application/MainActivity.java
@@ -1,13 +1,41 @@
 package com.mokshkhurana.mailchimp.application;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.mokshkhurana.mailchimpsdk.MailchimpSDK;
+import com.mokshkhurana.mailchimpsdk.model.Error;
+import com.mokshkhurana.mailchimpsdk.model.ListResponse;
+import com.mokshkhurana.mailchimpsdk.networking.Response;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private MailchimpSDK sdk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sdk = MailchimpSDK.getInstance();
+        sdk.initSDK("api_key");
+
+        getLists();
+    }
+
+    private void getLists() {
+        sdk.getLists(new Response<ListResponse>() {
+            @Override
+            public void onSuccess(ListResponse response) {
+                Log.d("Mailchimp", "Total items = " + response.getTotalItems());
+            }
+
+            @Override
+            public void onFailure(Error error) {
+                Log.d("Mailchimp", "Error = " + error.getDetail());
+            }
+        });
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# mailchimpsdk
-Wrapper for Mailchimp SDK v3
+# MailchimpSDK
+Wrapper for Mailchimp API v3 on android
+
+This library is still under development. Please use at your own risk.
+
+## Installation:
+
+```
+maven {
+url 'https://dl.bintray.com/mokshkhurana/moksh/'
+}
+
+dependencies {
+compile 'com.mokshkhurana:mailchimpsdk:1.0.0'
+}
+```
+## Usage
+
+```java
+MailchimpSDK sdk = MailchimpSDK.getInstance();
+sdk.initSDK("your_api_key");
+```
+
+## Current methods
+
+* Get lists
+* Get list by ID
+
+## Future Planning
+Next release: I will be completing post, patch and delete calls for list. After that I will be working account information.
